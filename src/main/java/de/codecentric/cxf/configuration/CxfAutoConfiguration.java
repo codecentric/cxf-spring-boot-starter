f0beb0bd366ef6dc95bc40f202c6d1f257aa39ef
Jonas Hecht <jonas.hecht@codecentric.de>
2016-09-15 09:14:06 +0200

    New Spring Boot 1.4 org.springframework.boot.web.servlet.ServletRegistrationBean

--- src/main/java/de/codecentric/cxf/configuration/CxfAutoConfiguration.java
+++ src/main/java/de/codecentric/cxf/configuration/CxfAutoConfiguration.java
@@ -8,7 +8,7 @@ import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
