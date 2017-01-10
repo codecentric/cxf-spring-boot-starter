461db069ea0be3e0ec9e878f9e6ab70b66c4295e
Jonas Hecht <jonas.hecht@codecentric.de>
2017-01-10 08:19:20 +0100

    #6 Deleted senseless import

--- src/test/java/de/codecentric/cxf/autodetection/WsdlScannerShould.java
+++ src/test/java/de/codecentric/cxf/autodetection/WsdlScannerShould.java
@@ -2,31 +2,18 @@ package de.codecentric.cxf.autodetection;
 
 
 import de.codecentric.cxf.common.BootStarterCxfException;
-import org.apache.maven.shared.invoker.*;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Optional;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 public class WsdlScannerShould {
