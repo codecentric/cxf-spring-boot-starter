6dc7f80d49af22a34021658c6fa8cd5b2e57ec43
jonashackt <jonas.hecht@codecentric.de>
2017-01-16 10:11:47 +0100

    #12 Provide the Elasticsearch API with the trace id ("X-B3-TraceId"), so that cxf-spring-boot-starter users can consume it (e.g. for providing the ID in SOAP tags for later easier traceability of errors).

--- src/main/java/de/codecentric/cxf/logging/ElasticsearchField.java
+++ src/main/java/de/codecentric/cxf/logging/ElasticsearchField.java
@@ -5,7 +5,8 @@ public enum ElasticsearchField {
     SOAP_METHOD_LOG_NAME("soap-method-name"),
     HTTP_HEADER_INBOUND("http-header-inbound"),
     SOAP_MESSAGE_INBOUND("soap-message-inbound"),
-    SOAP_MESSAGE_OUTBOUND("soap-message-outbound");
+    SOAP_MESSAGE_OUTBOUND("soap-message-outbound"),
+    SLEUTH_TRACE_ID("X-B3-TraceId");
 
     private String fieldname;
     
