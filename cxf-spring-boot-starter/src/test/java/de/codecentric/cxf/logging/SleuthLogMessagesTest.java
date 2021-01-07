57020a243bde68b89828f9af31affcd614469f7c
Jonas Hecht <jonas.hecht@codecentric.de>
2021-01-07 13:27:11 +0100

    Fixing SleuthLogMessagesTest by initializing SystemStubs correctly using the JUnit5 extension via `@ExtendWith(SystemStubsExtension.class)`. Also we don't need to add the system-stubs-core dependency, since system-stubs-jupiter gets that transitively for us.

--- cxf-spring-boot-starter/src/test/java/de/codecentric/cxf/logging/SleuthLogMessagesTest.java
+++ cxf-spring-boot-starter/src/test/java/de/codecentric/cxf/logging/SleuthLogMessagesTest.java
@@ -5,7 +5,6 @@ import de.codecentric.cxf.common.XmlUtils;
 import de.codecentric.namespace.weatherservice.WeatherException;
 import de.codecentric.namespace.weatherservice.WeatherService;
 import de.codecentric.namespace.weatherservice.general.GetCityForecastByZIP;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 import uk.org.webcompere.systemstubs.stream.SystemOut;
 
 import java.io.IOException;
@@ -23,6 +23,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(SystemStubsExtension.class)
 @SpringBootTest(
         classes = TestApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
@@ -42,25 +43,20 @@ public class SleuthLogMessagesTest {
     public void sleuth_meta_data_should_be_print_out() throws Exception {
         callSoapService();
 
-        String log = systemOut.getLines().toString();
-
-        assertTrue(isSleuthLogLineSomewhereIn(log));
+        assertTrue(isSleuthLogLineSomewhereIn(systemOut.getText()));
     }
 
     private boolean isSleuthLogLineSomewhereIn(String log) {
-        Pattern sleuth_log_line_pattern = Pattern.compile("(INFO \\[,\\w+,\\w+,.*: Returning a forecast.)");
+        Pattern sleuth_log_line_pattern = Pattern.compile("(INFO \\[,\\w+,\\w+.*: Returning a forecast.)");
         Matcher loglineMatcher = sleuth_log_line_pattern.matcher(log);
         return loglineMatcher.find();
     }
 
-    @Disabled
     @Test public void
     call_time_should_be_print_out() throws Exception {
         callSoapService();
 
-        String log = systemOut.getLines().toString();
-
-        assertThat(log, containsString("Call time"));
+        assertThat(systemOut.getText(), containsString("Call time"));
     }
 
     private void callSoapService() throws de.codecentric.cxf.common.BootStarterCxfException, IOException, WeatherException {
