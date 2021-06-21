18f7aefa1715e7da97422ea219d48d73e6e0394d
Jonas Hecht <jonas.hecht@codecentric.de>
2021-06-21 11:17:36 +0200

    Now that Heroku works again, let's reintegrate our Integration test :)

--- cxf-spring-boot-starter-samples/cxf-boot-simple-client/src/test/java/de/codecentric/soap/endpoint/WeatherServiceSoapClientIntegrationTest.java
+++ cxf-spring-boot-starter-samples/cxf-boot-simple-client/src/test/java/de/codecentric/soap/endpoint/WeatherServiceSoapClientIntegrationTest.java
@@ -29,7 +29,6 @@ public class WeatherServiceSoapClientIntegrationTest {
 		RestAssured.port = port;
 	}
 
-	@Disabled
 	@Test
 	public void callWeatherServiceSoapClient() {
 		WeatherResponse weatherResponse =
